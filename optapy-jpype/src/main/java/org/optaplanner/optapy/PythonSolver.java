6b2cb5f96eadbef9feb0887fd1b80cdc33a77e66
Christopher Chianelli <chris.chianelli@gmail.com>
2021-07-23 13:26:06 -0400

    Fixup README; make Dockerfile better for Binder, do not modify SolverConfig

--- optapy-jpype/src/main/java/org/optaplanner/optapy/PythonSolver.java
+++ optapy-jpype/src/main/java/org/optaplanner/optapy/PythonSolver.java
@@ -2,7 +2,6 @@ package org.optaplanner.optapy;
 
 import java.io.Serializable;
 
-import org.optaplanner.core.api.score.stream.ConstraintStreamImplType;
 import org.optaplanner.core.api.solver.Solver;
 import org.optaplanner.core.api.solver.SolverFactory;
 import org.optaplanner.core.config.solver.SolverConfig;
@@ -11,7 +10,6 @@ public class PythonSolver {
     public static Object solve(SolverConfig solverConfig, Serializable problem) {
         solverConfig = new SolverConfig().inherit(solverConfig);
         solverConfig.setClassLoader(PythonWrapperGenerator.gizmoClassLoader);
-        solverConfig.getScoreDirectorFactoryConfig().withConstraintStreamImplType(ConstraintStreamImplType.BAVET);
         Solver solver = SolverFactory.create(solverConfig).buildSolver();
         return solver.solve(PythonWrapperGenerator.wrap(solverConfig.getSolutionClass(), problem));
     }
