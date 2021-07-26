45ba57047d22e484466a73d886f1dd99b4701a3c
Christopher Chianelli <chris.chianelli@gmail.com>
2021-07-26 12:46:46 -0400

    Structure Changes

--- run-graal.sh
+++ run-graal.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 if which graalpython > /dev/null ; then
-    if [ ! -d optapy-school-timetabling/graalvenv ]; then
+    if [ ! -d optapy-quickstarts/school-timetabling/graalvenv ]; then
         echo "Creating venv"
-        graalpython -m venv optapy-school-timetabling/graalvenv
+        graalpython -m venv optapy-quickstarts/school-timetabling/graalvenv
     fi
-    cd optapy-graal
+    cd optapy-graal-core || { echo 'Unable to find the optapy-graal-core directory. Maybe it was accidently deleted?'; exit 1; }
     python -m build || { echo 'Build failed' ; exit 1; }
-    source ../optapy-school-timetabling/graalvenv/bin/activate
+    . ../optapy-quickstarts/school-timetabling/graalvenv/bin/activate
     pip uninstall -y optapy
     pip install dist/optapy-0.0.0-py3-none-any.whl
-    cd ../optapy-school-timetabling
+    cd ../optapy-quickstarts/school-timetabling || { echo 'Unable to find the optapy-quickstarts/school-timetabling directory. Maybe it was accidently deleted?'; exit 1; }
     python --jvm main.py
     deactivate
 else
