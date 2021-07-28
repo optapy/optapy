fdd6e7c22ef12720581dccbccc7c190b49be6d2f
Christopher Chianelli <chris.chianelli@gmail.com>
2021-07-28 15:01:44 -0400

    Do not change the environment mode to FULL_ASSERT for JPype

--- optapy-core/src/main/java/org/optaplanner/optapy/PythonSolver.java
+++ optapy-core/src/main/java/org/optaplanner/optapy/PythonSolver.java
@@ -1,17 +1,14 @@
 package org.optaplanner.optapy;
 
-import java.io.Serializable;
 import java.util.HashMap;
 
 import org.optaplanner.core.api.solver.Solver;
 import org.optaplanner.core.api.solver.SolverFactory;
-import org.optaplanner.core.config.solver.EnvironmentMode;
 import org.optaplanner.core.config.solver.SolverConfig;
 
 public class PythonSolver {
     public static Object solve(SolverConfig solverConfig, OpaquePythonReference problem) {
         solverConfig = new SolverConfig().inherit(solverConfig);
-        solverConfig.setEnvironmentMode(EnvironmentMode.FULL_ASSERT);
         solverConfig.setClassLoader(PythonWrapperGenerator.gizmoClassLoader);
         Solver solver = SolverFactory.create(solverConfig).buildSolver();
         return solver.solve(PythonWrapperGenerator.wrap(solverConfig.getSolutionClass(), problem, new HashMap<>()));
