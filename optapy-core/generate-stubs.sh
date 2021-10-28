b0cd2a076e5428cf6c5004a6c4417511eef7d522
Christopher Chianelli <chris.chianelli@gmail.com>
2021-10-28 11:11:02 -0400

    Remove stub generation build scripts since they are integrated into the python build

--- optapy-core/generate-stubs.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-mvn clean
-mvn dependency:copy-dependencies -Dstubs
-mvn dependency:copy-dependencies -Dstubs -Dclassifier=javadoc
-echo "version = '1.0'" > stubgenj/stubgenj/_version.py
-python create-stubs.py
-mvn clean
