5e31e707d49d2254eda59f8cc3ae28e026102f44
Christopher Chianelli <chris.chianelli@gmail.com>
2021-11-02 14:20:10 -0400

    Update stubgenj version, only generate stubs for API/Config packages

--- optapy-core/create-stubs.py
+++ optapy-core/create-stubs.py
@@ -7,9 +7,12 @@ jars = list(map(str, pathlib.Path('target/dependency').glob('**/*.jar')))
 jpype.startJVM(classpath=jars)
 
 import jpype.imports  # noqa
-import org.optaplanner  # noqa
+import org.optaplanner.core.api  # noqa
+import org.optaplanner.core.config  # noqa
+import java.lang # noqa
 import java.time # noqa
 import java.util # noqa
 
-stubgenj.generateJavaStubs([java.time, java.util, org.optaplanner], useStubsSuffix=True)
+stubgenj.generateJavaStubs([java.lang, java.time, java.util, org.optaplanner.core.api, org.optaplanner.core.config],
+                           useStubsSuffix=True)
 
