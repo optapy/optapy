b97c7642e942aa5131bb7589576641462fe91254
Christopher Chianelli <chris.chianelli@gmail.com>
2021-11-08 15:49:27 -0500

    Fix @inverse_relation_shadow_variable

--- optapy-core/src/main/python/optaplanner_java_interop.py
+++ optapy-core/src/main/python/optaplanner_java_interop.py
@@ -75,6 +75,9 @@ class PythonTriFunction:
 # ****************************************************************************
 
 
+# ****************************************************************************
+# PythonWrapperGenerator Python helper functions
+# ****************************************************************************
 def _get_python_object_id(item):
     """Returns a unique id for a Python Object (used in cloning)"""
     return id(item)
@@ -93,6 +96,7 @@ def _get_python_object_from_id(item_id):
 def _get_python_object_attribute(object_id, name):
     """Gets an attribute from a Python Object"""
     import java.lang.Object
+    import java.util.Collection
     import org.optaplanner.core.api.score.Score
     the_object = object_id
     python_object_getter = getattr(the_object, str(name))
@@ -101,14 +105,20 @@ def _get_python_object_attribute(object_id, name):
         error = (f'The attribute {name} on {object_id }is not callable (got {python_object_getter}, '
                  f'expecting a function). You might have overridden the function {name} with a value.')
         raise OptaPyException(error)
-    python_object = python_object_getter()
-    if python_object is None:
-        return None
-    elif isinstance(python_object, (str, bool, int, float, complex, org.optaplanner.core.api.score.Score)):
-        out = JObject(python_object, java.lang.Object)
-        return out
-    else:
-        return JProxy(org.optaplanner.optapy.OpaquePythonReference, inst=python_object, convert=True)
+    try:
+        python_object = python_object_getter()
+        if python_object is None:
+            return None
+        elif isinstance(python_object, (str, bool, int, float, complex, java.util.Collection,
+                                        org.optaplanner.core.api.score.Score)):
+            out = JObject(python_object, java.lang.Object)
+            return out
+        else:
+            return JProxy(org.optaplanner.optapy.OpaquePythonReference, inst=python_object, convert=True)
+    except Exception as e:
+        from org.optaplanner.optapy import OptaPyException # noqa
+        error = f'An exception occur when calling {str(name)} on {str(the_object)}: {str(e)}. Check the code.'
+        raise OptaPyException(error)
 
 
 def _get_python_array_to_id_array(the_object: List):
@@ -163,6 +173,61 @@ def _deep_clone_python_object(the_object: Any):
     return JProxy(org.optaplanner.optapy.OpaquePythonReference, inst=the_clone, convert=True)
 
 
+# ****************************************************************************
+# PythonList Python helper functions
+# ****************************************************************************
+"""
+    private static TriFunction<OpaquePythonReference, Integer, Integer, OpaquePythonReference> slicePythonList;
+"""
+
+
+def _clear_python_list(the_list: List):
+    the_list.clear()
+
+
+def _python_list_length(the_list: List):
+    return len(the_list)
+
+
+def _get_item_at_index_in_python_list(the_list: List, index: int):
+    return the_list[index]
+
+
+def _set_item_at_index_in_python_list(the_list: List, index: int, item: any):
+    the_list[index] = item
+
+
+def _add_item_to_python_list(the_list: List, item: any):
+    the_list.append(item)
+    return True
+
+
+def _add_item_at_index_in_python_list(the_list: List, index: int, item: any):
+    the_list.insert(index, item)
+    return True
+
+
+def _remove_item_from_python_list(the_list: List, item: any):
+    try:
+        the_list.remove(item)
+        return True
+    except ValueError:
+        return False
+
+
+def _remove_item_at_index_from_python_list(the_list: List, index: int):
+    the_list.pop(index)
+    return True
+
+
+def _does_python_list_contain_item(the_list: List, item: any):
+    return item in the_list
+
+
+def _slice_python_list(the_list: List, start: int, end: int):
+    return the_list[start:end]
+
+
 def init(*args, path: List[str] = None, include_optaplanner_jars: bool = True, log_level='INFO'):
     """Start the JVM. Throws a RuntimeError if it is already started.
 
@@ -189,7 +254,7 @@ def init(*args, path: List[str] = None, include_optaplanner_jars: bool = True, l
     import java.util.function.Function
     import java.util.function.BiFunction
     import org.optaplanner.core.api.function.TriFunction
-    from org.optaplanner.optapy import PythonWrapperGenerator, PythonPlanningSolutionCloner # noqa
+    from org.optaplanner.optapy import PythonWrapperGenerator, PythonPlanningSolutionCloner, PythonList # noqa
     PythonWrapperGenerator.setPythonObjectToId(JObject(PythonFunction(_get_python_object_id),
                                                        java.util.function.Function))
     PythonWrapperGenerator.setPythonObjectToString(JObject(PythonFunction(_get_python_object_str),
@@ -204,8 +269,28 @@ def init(*args, path: List[str] = None, include_optaplanner_jars: bool = True, l
         JObject(PythonBiFunction(_get_python_object_attribute), java.util.function.BiFunction))
     PythonWrapperGenerator.setPythonObjectIdAndAttributeSetter(JObject(PythonTriFunction(_set_python_object_attribute),
                                                                        org.optaplanner.core.api.function.TriFunction))
+
     PythonPlanningSolutionCloner.setDeepClonePythonObject(JObject(PythonFunction(_deep_clone_python_object),
                                                                   java.util.function.Function))
+
+    PythonList.setClearPythonList(JObject(PythonFunction(_clear_python_list), java.util.function.Function))
+    PythonList.setGetPythonListLength(JObject(PythonFunction(_python_list_length), java.util.function.Function))
+    PythonList.setGetItemAtIndexInPythonList(JObject(PythonBiFunction(_get_item_at_index_in_python_list),
+                                                     java.util.function.BiFunction))
+    PythonList.setSetItemAtIndexInPythonList(JObject(PythonTriFunction(_set_item_at_index_in_python_list),
+                                                     org.optaplanner.core.api.function.TriFunction))
+    PythonList.setAddItemToPythonList(JObject(PythonBiFunction(_add_item_to_python_list),
+                                              java.util.function.BiFunction))
+    PythonList.setAddItemAtIndexInPythonList(JObject(PythonTriFunction(_add_item_at_index_in_python_list),
+                                                     org.optaplanner.core.api.function.TriFunction))
+    PythonList.setRemoveItemFromPythonList(JObject(PythonBiFunction(_remove_item_from_python_list),
+                                                   java.util.function.BiFunction))
+    PythonList.setRemoveItemAtIndexFromPythonList(JObject(PythonBiFunction(_remove_item_at_index_from_python_list),
+                                                          java.util.function.BiFunction))
+    PythonList.setDoesPythonListContainItem(JObject(PythonBiFunction(_does_python_list_contain_item),
+                                                    java.util.function.BiFunction))
+    PythonList.setSlicePythonList(JObject(PythonTriFunction(_slice_python_list),
+                                          org.optaplanner.core.api.function.TriFunction))
 
 
 def ensure_init():
@@ -396,7 +481,7 @@ def _to_java_list(python_list: List):
     return out
 
 
-def _get_optaplanner_annotations(python_class: Type) -> List[Tuple[str, JClass, List[dict]]]:
+def _get_optaplanner_annotations(python_class: Type) -> List[Tuple[str, JClass, str, List[dict]]]:
     """Gets the methods with OptaPlanner annotations in the given class"""
     method_list = [attribute for attribute in dir(python_class) if callable(getattr(python_class, attribute)) and
                    attribute.startswith('__') is False]
@@ -405,9 +490,10 @@ def _get_optaplanner_annotations(python_class: Type) -> List[Tuple[str, JClass, 
         optaplanner_annotations = [attribute for attribute in dir(getattr(python_class, method)) if
                                    attribute.startswith('__optaplanner')]
         if optaplanner_annotations:
-            return_type = getattr(getattr(python_class, method), "__return", None)
+            return_type = getattr(getattr(python_class, method), "__optapy_return", None)
+            method_signature = getattr(getattr(python_class, method), "__optapy_signature", None)
             annotated_methods.append(
-                _to_java_list([method, return_type,
+                _to_java_list([method, return_type, method_signature,
                                _to_java_list(list(map(lambda annotation: getattr(getattr(python_class, method),
                                                                                  annotation), optaplanner_annotations)))
                                ]))
@@ -418,8 +504,8 @@ def get_class(python_class: Union[Type, Callable]) -> JClass:
     """Return the Java Class for the given Python Class"""
     if isinstance(python_class, jpype.JClass):
         return cast(JClass, python_class)
-    if hasattr(python_class, '__javaClass'):
-        return python_class.__javaClass
+    if hasattr(python_class, '__optapy_java_class'):
+        return python_class.__optapy_java_class
     if python_class == int:
         from java.lang import Integer
         return cast(JClass, Integer)
@@ -447,7 +533,7 @@ def _generate_problem_fact_class(python_class):
     from org.optaplanner.optapy import PythonWrapperGenerator # noqa
     optaplanner_annotations = _get_optaplanner_annotations(python_class)
     parent_class = None
-    if len(python_class.__bases__) == 1 and hasattr(python_class.__bases__[0], '__javaClass'):
+    if len(python_class.__bases__) == 1 and hasattr(python_class.__bases__[0], '__optapy_java_class'):
         parent_class = get_class(python_class.__bases__[0])
     out = PythonWrapperGenerator.defineProblemFactClass(python_class.__name__ + str(unique_class_id),
                                                         parent_class,
@@ -462,7 +548,7 @@ def _generate_planning_entity_class(python_class: Type, annotation_data: Dict[st
     from org.optaplanner.optapy import PythonWrapperGenerator # noqa
     optaplanner_annotations = _get_optaplanner_annotations(python_class)
     parent_class = None
-    if len(python_class.__bases__) == 1 and hasattr(python_class.__bases__[0], '__javaClass'):
+    if len(python_class.__bases__) == 1 and hasattr(python_class.__bases__[0], '__optapy_java_class'):
         parent_class = get_class(python_class.__bases__[0])
     out = PythonWrapperGenerator.definePlanningEntityClass(python_class.__name__ + str(unique_class_id),
                                                            parent_class,
